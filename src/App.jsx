7e8df9e91462b78e55171737d4dcee0355ae23ee
edersito19 <ederquezada15@gmail.com>
2024-04-22 21:47:14 -0600

    componente card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import  Header from "../src/components/Header";
+import  Card from "../src/components/Card";
+
 function App() {
   return (
     <>
     <Header/>
+    <Card/>
       <h2 className="text-[#2E3057] text-4xl font-bold text-center">
      Codigo de prueba 
         </h2>
