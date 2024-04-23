db0d04e659ebdf79d4e38c71e2a8fd6678468cb1
edersito19 <ederquezada15@gmail.com>
2024-04-22 22:16:15 -0600

    Cree pagina Error404

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,11 @@
+export default function Error404() {
+  return (
+    <section className="py-0 px-5 relative">
+      <article className="max-w-[1000px] mx-auto relative pb-8 top-0 left-0 w-full h-screen flex flex-col justify-center items-center gap-4">
+        <h2 className="text-[#2E3057] text-4xl font-bold text-center">
+          Error, no se ha podido encontrar esta página
+        </h2>
+      </article>
+    </section>
+  );
+}
