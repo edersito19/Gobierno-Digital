cee8f6135620372733e7aaed69cf801762244144
edersito19 <ederquezada15@gmail.com>
2024-04-22 22:21:45 -0600

    Cree componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+      <footer className="bg-[#78716c1a] py-12 px-5">
+        <div className="max-w-[1200px] mx-auto">
+          <div className="flex gap-2">
+            <h2 className="text-4xl font-bold">Pokédex</h2>
+          </div>
+        </div>
+      </footer>
+    );
+  }
